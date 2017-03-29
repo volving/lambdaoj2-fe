e6dd26a24564e7a528306c130fa9524d39fb2ecc
volving <volving@icloud.com>
2017-03-29 17:53:48 +0800

    Mdf: lorem -> sample text

--- src/components/oj/Problems.jsx
+++ src/components/oj/Problems.jsx
@@ -11,11 +11,11 @@ export default class Problems extends React.Component {
         return (
             <div>
                 <h1>Problems</h1>
-                <Pro name="Alpha"></Pro>
-                <Pro name="Beta"></Pro>
-                <Pro name="Gamma"></Pro>
-                <Pro name="Epsilon"></Pro>
-                <Pro name="Pi"></Pro>
+                <Pro name="实验十： 骰子日历"></Pro>
+                <Pro name="实验九：最大子数组问题"></Pro>
+                <Pro name="实验八：超定方程组的求解"></Pro>
+                <Pro name="实验七：序列求和问题"></Pro>
+                <Pro name="实验六：生成英文回文串"></Pro>
             </div>
         );
     }
